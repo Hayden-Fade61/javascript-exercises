30423ab9d5e37252c30c9afb1298258247f84a12
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-09 14:02:55 +0200

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return roundToPrecision((temp - 32 ) / 1.8, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return roundToPrecision((temp * 1.8 ) +32, 1);
 };
+
+function roundToPrecision(number, precision){
+  let base = 10 ** precision;
+  return Math.round(number * base) / base; 
+}
 
 // Do not edit below this line
 module.exports = {
