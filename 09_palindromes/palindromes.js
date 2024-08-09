3077ae00cd348b1bef7fbc63e059befe12930754
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-09 17:23:58 +0200

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[^\w]+/g, "");
+  str = str.toLowerCase();
+  console.log(str);
+  let j = str.length - 1;
+  let i = 0;
+  for(; i < str.length && j >= 0; i++, j--){
+    if(str.charAt(i) != str.charAt(j)) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
