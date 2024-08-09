76c7a6fad5ed331e15a1afab68a1c10bb162c6a2
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-09 17:34:24 +0200

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  // Base cases
+  if(n<0) return "OOPS";
+  if (n==0) return 0;
+  if (n==1) return 1;
+  // Recursive step 
+  return fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line
