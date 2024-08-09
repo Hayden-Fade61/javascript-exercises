8d70737ddb451dff01f386279ccc67faa5dba296
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-09 16:55:33 +0200

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+	arr.forEach(element => {
+    result += element;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let result = 1;
+  args.forEach(element => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1;
+  for(let i = 0; i<b; i++){
+    result = multiply([result, a]);
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+	for(let i = 1; i<=a; i++){
+    result *= i;
+  };
+  return result;
 };
 
 // Do not edit below this line
