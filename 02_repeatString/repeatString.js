3418bf607c04adcbe7cf5ada3a4f5f6cbfb33ff5
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-09 12:28:51 +0200

    02 Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(repeatUnit, numRepeats) {
+    let result = "";
+    if(result == repeatUnit){
+        return result;
+    }
+    if(numRepeats < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < numRepeats; i++){
+        result += repeatUnit;
+    }
+    return result;
 };
 
 // Do not edit below this line
