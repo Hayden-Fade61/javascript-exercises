6039dc15ebad4811ec7d391cd03cafc2ed83e0f8
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-09 13:08:08 +0200

    04 removed console.logs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,6 @@
 const removeFromArray = function(arr, ...args) {
     args.forEach(element => {
         for(let i = 0; i < arr.length; i++){
-            console.log(`i: ${i}`);
-            console.log(`arr: ${arr[i]}`);
-            console.log(`arg: ${element}`);
             if(element === arr[i]){
                 arr.splice(i--, 1);
             }
