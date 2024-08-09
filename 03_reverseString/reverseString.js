5d6236da49c6f5a1813b3649a9eb11f6c19ff1f3
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-09 12:34:06 +0200

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reversed = "";
+    for(let i = input.length - 1; i >= 0; i--){
+        reversed += input.charAt(i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
