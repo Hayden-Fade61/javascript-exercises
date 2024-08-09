8ff3f3f8d1a0c2608fd4264fc45af4228ab8eb69
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-09 13:07:17 +0200

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    args.forEach(element => {
+        for(let i = 0; i < arr.length; i++){
+            console.log(`i: ${i}`);
+            console.log(`arr: ${arr[i]}`);
+            console.log(`arg: ${element}`);
+            if(element === arr[i]){
+                arr.splice(i--, 1);
+            }
+        }
+    });
+    return arr;
 };
 
 // Do not edit below this line
