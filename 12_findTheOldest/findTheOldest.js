6bcb7159ee3abb2a73a634c5d3b7d06f526b1b12
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-10 02:40:42 +0200

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    return getAge(currentPerson) > getAge(oldestPerson) ? currentPerson : oldestPerson;
+  });
 };
+
+function getAge(person){
+  const currentYear = new Date().getFullYear();
+  return person.yearOfDeath === undefined ? 
+    currentYear - person.yearOfBirth :
+    person.yearOfDeath - person.yearOfBirth ; 
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
