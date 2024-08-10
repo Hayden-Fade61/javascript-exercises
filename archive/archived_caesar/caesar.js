dd3f85b65a5e1aab255c22397cfa62ac8e90a46b
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-10 05:12:52 +0200

    Caesar Cipher complete

--- archive/archived_caesar/caesar.js
+++ archive/archived_caesar/caesar.js
@@ -15,15 +15,15 @@ const caesar = function(msg, shift) {
 
 function doShift(char, shift){
   const LOWERCASE_MIN = "a".charCodeAt(0);
-  const LOWERCASE_MAX = "z".charCodeAt(0);
   const ALPHABET_LENGTH = 26;
-  const EFFECTIVE_SHIFT = shift % ALPHABET_LENGTH;
-  const isUppercase = char == char.toUpperCase();
+  const EFFECTIVE_SHIFT = shift >= 0 ? shift % ALPHABET_LENGTH : 
+    ALPHABET_LENGTH + (shift % ALPHABET_LENGTH);
+  const IS_UPPERCASE = char == char.toUpperCase();
   let charPos = char.toLowerCase().charCodeAt(0) - LOWERCASE_MIN;
   let shiftedPos = (charPos + EFFECTIVE_SHIFT) % ALPHABET_LENGTH;
   let shiftedChar = '';
   shiftedChar = String.fromCharCode(LOWERCASE_MIN + shiftedPos);
-  return isUppercase ? shiftedChar.toUpperCase() : shiftedChar;
+  return IS_UPPERCASE ? shiftedChar.toUpperCase() : shiftedChar;
 }
 
 // Do not edit below this line
