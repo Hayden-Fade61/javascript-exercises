46a708aa7f04f43a62bbb9b75e5ee890b41d2570
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-10 02:21:55 +0200

    11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let result = Array(books.length);
+  return books.map((book) => book.title);
 };
 
 // Do not edit below this line
